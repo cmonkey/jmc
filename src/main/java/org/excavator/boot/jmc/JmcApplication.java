087b20b63a6c7294f3fbb22e4b60fa3f678f0844
CMonkey <42.codemonkey@gmail.com>
2021-08-04 10:41:23 +0800

    init project

---
+++ src/main/java/org/excavator/boot/jmc/JmcApplication.java
@@ -0,0 +1,13 @@
+package org.excavator.boot.jmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/excavator/boot/jmc/JmcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.excavator.boot.jmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
